dceb4d9fa2b9c72a7317fe0a2390e4cf5f270f15
RobertAKARobin <robertgfthomas@gmail.com>
2015-05-20 11:08:58 -0400

    Adding in mongo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io');
 var io = io(http);
 
+mongoose.connect("mongodb://localhost/test");
 app.set("view engine", "hbs");
 
 app.get('/', function(req, res){
