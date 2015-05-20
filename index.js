decc1cc10252659a1ef0fd96b1f94a772a569489
RobertAKARobin <robertgfthomas@gmail.com>
2015-05-20 13:42:09 -0400

    It persists

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ var io = require('socket.io');
 var io = io(http);
 
 mongoose.connect("mongodb://localhost/test");
+var db = mongoose.connection;
+var Chat = mongoose.model("Chat", new mongoose.Schema({
+  content: "string"
+}));
+
 app.set("view engine", "hbs");
 
 app.get('/', function(req, res){
-  res.render("index");
+  var query = Chat.find().lean().exec(function(err, chats){
+    res.render("index", {chats: chats});
+  });
 });
 
 io.on('connection', function(socket){
+
   socket.on("chatsend", function(message){
     io.emit("chatpost", message);
+    var chat = new Chat({ content: message });
+    chat.save();
   });
+
+  socket.on("chatdelete", function(id){
+    Chat.find({_id: id}).remove().exec();
+  });
+
 });
 
 http.listen(3000);
